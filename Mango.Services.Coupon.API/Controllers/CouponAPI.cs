0f62dcb0d87b9b4860e8343803e138eac162a91a
PRANEETHNS <77009009+PRANEETHNS@users.noreply.github.com>
2025-08-24 09:15:14 +1000

    Endpoint creation

---
+++ Mango.Services.Coupon.API/Controllers/CouponAPI.cs
@@ -0,0 +1,53 @@
+﻿using Mango.Services.Coupon.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Mango.Services.Coupon.API.Models;
+
+namespace Mango.Services.Coupon.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPI : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public CouponAPI(AppDbContext db) 
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            try { 
+                IEnumerable<API.Models.Coupon> objList = _db.Coupons.ToList();
+                return objList;
+            }
+            catch(Exception ex)
+            {
+            
+            }
+
+            return null;
+        
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                API.Models.Coupon objList = _db.Coupons.FirstOrDefault(u=>u.CouponId==id);
+                return objList;
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return null;
+
+        }
+
+    }
+}
